426be7ea31cad3949ea6e79207614e5d0f45a624
Heather Anderson <heather@thisisnotanorg.org>
2015-01-19 20:28:50 +0000

    Add first working spec.

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,15 +1,10 @@
-class String
-	define_method(:word_frequency) do
-		self = phrase
-		phrase_array = phrase.split()
-		times = phrase_array#length
-		
-		times.each do |find|
-			
-			
-			
-		
-		end
-		duplicates
+class String	
+	define_method(:word_frequency) do |word_to_find|
+		phrase = self.split()
+	 	word_to_find = [""]
+		word_frequency = phrase.count(word_to_find)
+		word_frequency.+(1)
 	end
-end	
+end
+		
+		
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -1,15 +1,10 @@
 require('rspec')
 require('word_frequency')
 
-describe('String#word_frequency') do
-	
-	it("splits a string into an array") do
-		expect(("i love you").split()).to(eq(["i", "love", "you"]))
-	end
-	it("finds duplicate words in an array and counts the number of instances") do
-		expect(("i").word_frequency()).to(eq(1))
-	end
-	it("when given the phrase 'love love love', with the variable 'love', it should return 3") do
-		expect(("love love love").word_frequency()).to(eq(3))
-	end
+describe("String#word_frequency") do
+		it("for a string with only one word, counts if the given word appears in the string") do
+			phrase = ("cat")
+			word_to_find = ("cat")
+			expect(phrase.word_frequency(word_to_find)).to(eq(1))
+  end
 end
