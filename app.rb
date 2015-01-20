85b40e45191460425822c9de8629794ab80d2855
Heather Anderson <heather@thisisnotanorg.org>
2015-01-20 01:31:47 +0000

    Add app functionality.

--- app.rb
+++ app.rb
@@ -1,17 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/rps.rb')
+require('./lib/word_frequency.rb')
 
 get('/form') do
-	@word = params.fetch('word')
-	@phrase = params.fetch('phrase')
   erb(:form)
 end
 
 get('/result') do
 	@word = params.fetch('word')
 	@phrase = params.fetch('phrase')
-	@totalnumber = params.fetch('phrase').scrabble(params.fetch('word')
+	@totalnumber = params.fetch('phrase').word_frequency(params.fetch('word'))
   erb(:result)
 end
